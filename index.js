904009939dbd68a70cf1a9aa39792a8b3a602f14
Jim Calvin <2394328+jim-calvin@users.noreply.github.com>
2019-10-23 15:36:57 -0400

    Added optional Dew Point service
    Also added optional temperature and humidity calibration offsets as part of the accessory configuration

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ class SHT3xAccessory {
     this.category = Accessory.Categories.SENSOR
     this.interval = config.interval || 60
     this.historyOptions = config.history || {}
+    this.temperatureCalibration = config.temperatureCalibration || 0
+    this.humidityCalibration = config.humidityCalibration || 0
+    this.dewpointEnabled = config.dewpointEnabled || false
 
+	this.log(`Temperature calibration: ${this.temperatureCalibration}, Humidity calibration: ${this.humidityCalibration}, dewpointEnabled: ${this.dewpointEnabled}`)
     const address = parseInt(config.address, 16) || 0x44
     const bus = config.bus || 1
 
@@ -23,12 +27,23 @@ class SHT3xAccessory {
     this.sensor = new SHT3xSensor(address, bus)
   }
 
+  calculateDewpoint(temperature, humidity) {
+	return (humidity/100.0)**0.125*(112.0+0.9*temperature) + 0.1*temperature-112.0
+  }
+  
   pollSensorData () {
     this.sensor.readSensorData().then((data) => {
-      const {temperature, humidity} = data
-
-      this.log(`Humidity: ${humidity.toFixed(2)} %, Temperature: ${temperature.toFixed(2)} °C`)
-      this.historyService.addEntry({time: moment().unix(), temp: temperature, humidity: humidity})
+      var {temperature, humidity} = data
+      temperature = temperature + this.temperatureCalibration
+      humidity = humidity + this.humidityCalibration
+	  if (this.dewpointEnabled) {
+	    const dewpoint = this.calculateDewpoint(temperature, humidity)
+        this.log(`Humidity: ${humidity.toFixed(2)}%, Temperature: ${temperature.toFixed(2)}°C, Dewpoint: ${dewpoint.toFixed(2)}°C`)
+        this.historyService.addEntry({time: moment().unix(), temp: temperature, humidity: humidity, dewpoint: dewpoint})
+    } else {
+        this.log(`Humidity: ${humidity.toFixed(2)}%, Temperature: ${temperature.toFixed(2)}°C`)
+        this.historyService.addEntry({time: moment().unix(), temp: temperature, humidity: humidity})
+      }
 
       this.data = data
     }).catch(err => this.log(err.message))
@@ -42,6 +57,20 @@ class SHT3xAccessory {
     }
   }
 
+  getDewPoint (dataFunction, callback) {
+    this.sensor.readSensorData().then((data) => {
+      var {temperature, humidity} = data
+      if (this.data === undefined) {
+        callback(new Error('No data'))
+      } else {
+        temperature = temperature + this.temperatureCalibration
+        humidity = humidity + this.humidityCalibration
+        const dewpoint = this.calculateDewpoint(temperature, humidity)
+        callback(null, dataFunction(dewpoint))
+      }
+      }).catch(err => this.log(err.message))
+  }
+
   getServices () {
     const informationService = new Service.AccessoryInformation()
     informationService
@@ -50,16 +79,25 @@ class SHT3xAccessory {
       .setCharacteristic(Characteristic.SerialNumber, `${os.hostname()}-${this.displayName}`)
       .setCharacteristic(Characteristic.FirmwareRevision, packageVersion)
 
-    const temperatureService = new Service.TemperatureSensor()
+    const temperatureService = new Service.TemperatureSensor("Temperature", "temperatureService")
     temperatureService
       .getCharacteristic(Characteristic.CurrentTemperature)
       .setProps({minValue: -40, maxValue: 125})
       .on('get', this.getSensorData.bind(this, data => data.temperature))
 
-    const humidityService = new Service.HumiditySensor()
+    const humidityService = new Service.HumiditySensor("Humidity", "humidityService")
     humidityService
       .getCharacteristic(Characteristic.CurrentRelativeHumidity)
       .on('get', this.getSensorData.bind(this, data => data.humidity))
+
+    var dewPointService
+    if (this.dewpointEnabled) {
+      dewPointService = new Service.TemperatureSensor('Dew Point', 'dewPointService')
+      dewPointService
+        .getCharacteristic(Characteristic.CurrentTemperature)
+        .setProps({minValue: -40, maxValue: 125})
+        .on('get', this.getDewPoint.bind(this, data => data))
+    }
 
     humidityService.isPrimaryService = true
     humidityService.linkedServices = [temperatureService]
@@ -69,7 +107,11 @@ class SHT3xAccessory {
     setInterval(this.pollSensorData.bind(this), this.interval * 1000)
     this.pollSensorData()
 
-    return [informationService, temperatureService, humidityService, this.historyService]
+	var services = [informationService, temperatureService, humidityService, this.historyService]
+	if (this.dewpointEnabled) {
+	  services = [informationService, temperatureService, humidityService, dewPointService, this.historyService]
+	}
+    return services
   }
 }
 
